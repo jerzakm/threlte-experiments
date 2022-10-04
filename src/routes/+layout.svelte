db702a60e9fa5e218d153bfeee679583f3f5035b
Marcin Jerzak <hey@jerzakm.com>
2022-10-04 21:45:24 +0200

    sidebar formatting - active link is now highlighted

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import '../app.postcss';
 	import '../styles/global.css';
 
+	import { page } from '$app/stores';
+
+	console.log();
+
 	const nav = [
 		{
 			name: '3D fantasy cards.',
@@ -13,7 +17,6 @@
 <nav class="drawer drawer-mobile">
 	<input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
 	<main class="drawer-content flex flex-col items-center justify-center">
-		<!-- Page content here -->
 		<slot />
 		<label
 			for="my-drawer-2"
@@ -23,9 +26,13 @@
 	<nav class="drawer-side">
 		<label for="my-drawer-2" class="drawer-overlay" />
 		<ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-			<h1 class="font-bold text-lg">Stuff made with Threlte:</h1>
-			{#each nav as { name, href }}
-				<li><a {href}>{name}</a></li>
+			<h1 class="font-bold text-lg mb-2">Stuff made with Threlte:</h1>
+			{#each nav as { name, href }, i}
+				<li>
+					<a class={`${'/' + href == $page.url.pathname ? 'active' : ''}`} {href}
+						>{i + 1} - <b>{name}</b></a
+					>
+				</li>
 			{/each}
 		</ul>
 	</nav>
