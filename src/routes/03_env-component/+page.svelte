9da70cff8c0f464cbb6874fa1a1e5b179b93433d
Marcin Jerzak <jerzakm@gmail.com>
2022-10-11 21:09:49 +0200

    knobby

--- src/routes/03_env-component/+page.svelte
+++ src/routes/03_env-component/+page.svelte
@@ -1,10 +1,50 @@
 <script lang="ts">
+	import Environment from '$lib/components/Environment.svelte';
 	import { Canvas } from '@threlte/core';
+	import * as knobby from 'svelte-knobby';
 	import Scene from './Scene.svelte';
+
+	const controls = knobby.panel({
+		// labelled control panels are collapsible
+		$label: 'Options',
+
+		environmentEnabled: true,
+		isBackground: true,
+
+		// functions become buttons. if state is returned, it will
+		// update the store
+		increment: (value) => ({
+			// ...value,
+			// clicks: value.clicks + 1
+		})
+	});
+
+	// the returned store is writable
+	$controls.message = 'Hello Knobby!';
 </script>
 
 <wrapper class="relative h-full">
 	<Canvas>
+		{#if $controls.environmentEnabled}
+			<!-- <Environment
+		path={'03_env/milkyway_cube/'}
+		files={[
+			'dark-s_nx.jpg',
+			'dark-s_ny.jpg',
+			'dark-s_nz.jpg',
+			'dark-s_px.jpg',
+			'dark-s_py.jpg',
+			'dark-s_pz.jpg'
+		]}
+		isCubeMap
+		isBackground
+	/> -->
+			<Environment
+				path={'03_env/'}
+				files={'equirect_ruined_room.jpg'}
+				isBackground={$controls.isBackground}
+			/>
+		{/if}
 		<Scene />
 	</Canvas>
 </wrapper>
--- src/routes/03_env-component/Scene.svelte
+++ src/routes/03_env-component/Scene.svelte
@@ -1,40 +1,17 @@
 <script lang="ts">
-	import { Group, Mesh, OrbitControls, PerspectiveCamera, useFrame } from '@threlte/core';
+	import { Group, Mesh, OrbitControls, PerspectiveCamera } from '@threlte/core';
 	import { MeshStandardMaterial, SphereGeometry } from 'three';
-	import Environment from '$lib/components/Environment.svelte';
 
 	let spheres = 15;
-
-	let env = true;
-
-	$: console.log(env);
 </script>
 
 <PerspectiveCamera position={{ x: 5, y: 15, z: 5 }} fov={60} near={1} far={20000}>
 	<OrbitControls autoRotate={false} enableZoom={true} target={{ x: 0, y: 0, z: 0 }} />
 </PerspectiveCamera>
 
-{#if env}
-	<Environment
-		path={'03_env/milkyway_cube/'}
-		files={[
-			'dark-s_nx.jpg',
-			'dark-s_ny.jpg',
-			'dark-s_nz.jpg',
-			'dark-s_px.jpg',
-			'dark-s_py.jpg',
-			'dark-s_pz.jpg'
-		]}
-		isCubeMap
-		isBackground
-	/>
-	<!-- <Environment path={'03_env/'} files={'equirect_ruined_room.jpg'} isBackground /> -->
-{/if}
-
 <Group>
 	{#each { length: spheres } as sphere, i}
 		<Mesh
-			on:click={() => (env = !env)}
 			geometry={new SphereGeometry(2)}
 			material={new MeshStandardMaterial({
 				color: 0xffffff,
@@ -42,7 +19,6 @@
 				roughness: 0,
 				envMapIntensity: 1
 			})}
-			interactive
 			castShadow
 			receiveShadow
 			position={{ x: Math.random() * 3 * i, y: 3, z: Math.random() * 3 * i }}
