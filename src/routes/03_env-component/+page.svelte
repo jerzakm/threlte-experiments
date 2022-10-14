e83d777a69e86d47ad2e2821fd91111d6d9df524
Marcin Jerzak <jerzakm@gmail.com>
2022-10-14 13:41:11 +0200

    knobby tweaks: - enabled env scene swapping

--- src/routes/03_env-component/+page.svelte
+++ src/routes/03_env-component/+page.svelte
@@ -4,6 +4,10 @@
 	import * as knobby from 'svelte-knobby';
 	import Scene from './Scene.svelte';
 
+	let path = '03_env/';
+	let files: string | [string, string, string, string, string, string] = 'equirect_ruined_room.jpg';
+	let isCubeMap = false;
+
 	const controls = knobby.panel({
 		// labelled control panels are collapsible
 		$label: 'Options',
@@ -13,10 +17,23 @@
 
 		// functions become buttons. if state is returned, it will
 		// update the store
-		increment: (value) => ({
-			// ...value,
-			// clicks: value.clicks + 1
-		})
+		cubicJpgScene: () => {
+			isCubeMap = true;
+			path = '03_env/milkyway_cube/';
+			files = [
+				'dark-s_nx.jpg',
+				'dark-s_ny.jpg',
+				'dark-s_nz.jpg',
+				'dark-s_px.jpg',
+				'dark-s_py.jpg',
+				'dark-s_pz.jpg'
+			];
+		},
+		equirectJpgScene: () => {
+			isCubeMap = false;
+			path = '03_env/';
+			files = 'equirect_ruined_room.jpg';
+		}
 	});
 
 	// the returned store is writable
@@ -26,24 +43,7 @@
 <wrapper class="relative h-full">
 	<Canvas>
 		{#if $controls.environmentEnabled}
-			<!-- <Environment
-		path={'03_env/milkyway_cube/'}
-		files={[
-			'dark-s_nx.jpg',
-			'dark-s_ny.jpg',
-			'dark-s_nz.jpg',
-			'dark-s_px.jpg',
-			'dark-s_py.jpg',
-			'dark-s_pz.jpg'
-		]}
-		isCubeMap
-		isBackground
-	/> -->
-			<Environment
-				path={'03_env/'}
-				files={'equirect_ruined_room.jpg'}
-				isBackground={$controls.isBackground}
-			/>
+			<Environment {path} {files} isBackground={$controls.isBackground} {isCubeMap} />
 		{/if}
 		<Scene />
 	</Canvas>
