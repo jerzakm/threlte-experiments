45eb01df55d055b87953fc2444633e282a02b8bc
jerzakm <jerzakm@gmail.com>
2022-12-25 15:59:26 +0100

    martini UVs

--- src/routes/10_martini/Scene.svelte
+++ src/routes/10_martini/Scene.svelte
@@ -1,95 +1,11 @@
 <script lang="ts">
+	import { decodeTerrainFromTile, genMartiniTerrain } from '$lib/martiniTerrain';
 	import { OrbitControls, T, useFrame, useThrelte } from '@threlte/core';
-	import { Color, MeshStandardMaterial } from 'three';
-	import { DEG2RAD } from 'three/src/math/MathUtils';
-	import {
-		AutoColliders,
-		Collider,
-		CollisionGroups,
-		Debug,
-		RigidBody,
-		World
-	} from '@threlte/rapier';
-	import { default as Martini } from '@mapbox/martini';
-	import { BufferGeometry } from 'three';
-	import { BufferAttribute } from 'three';
-	import { DoubleSide } from 'three';
-	import { Environment } from '@threlte/extras';
+	import { AutoColliders, Collider, RigidBody, World } from '@threlte/rapier';
+	import { BufferAttribute, BufferGeometry, Color, DoubleSide, MeshStandardMaterial } from 'three';
 
+	import { PCFSoftShadowMap, Vector3 } from 'three';
 	import CSM from 'three-csm';
-	import { Vector3 } from 'three';
-	import { PCFSoftShadowMap } from 'three';
-
-	const decodeTerrainFromTile = (tileImg: HTMLImageElement) => {
-		// ! todo move all this logic to worker thread
-		const tileSize = tileImg.width;
-
-		// 1. Draw terrain on an offscreen canvas
-		const textureCanvas = new OffscreenCanvas(tileSize, tileSize);
-		textureCanvas.height = screen.height;
-		textureCanvas.width = screen.width;
-		const ctx = textureCanvas.getContext('2d');
-
-		if (!ctx) {
-			return;
-		}
-		ctx.drawImage(tileImg, 0, 0);
-		const data = ctx.getImageData(0, 0, tileSize, tileSize).data;
-
-		const gridSize = tileSize + 1;
-
-		const terrain = new Float32Array(gridSize * gridSize);
-
-		// 2. Decode terrain values from rgb terrain tile
-		for (let y = 0; y < tileSize; y++) {
-			for (let x = 0; x < tileSize; x++) {
-				const k = (y * tileSize + x) * 4;
-				const r = data[k + 0];
-				const g = data[k + 1];
-				const b = data[k + 2];
-				const a = data[k + 3];
-				terrain[y * gridSize + x] = (r * 256 * 256 + g * 256.0 + b) / 10.0 - 10000.0;
-			}
-		}
-		// 2.1 backfill right and bottom borders
-		for (let x = 0; x < gridSize - 1; x++) {
-			terrain[gridSize * (gridSize - 1) + x] = terrain[gridSize * (gridSize - 2) + x];
-		}
-		for (let y = 0; y < gridSize; y++) {
-			terrain[gridSize * y + gridSize - 1] = terrain[gridSize * y + gridSize - 2];
-		}
-
-		return terrain;
-	};
-
-	const genMartiniTerrain = (terrain: Float32Array, size: number, terrainError: 10) => {
-		const martiniInstance = new Martini(size + 1);
-
-		const tile = martiniInstance.createTile(terrain);
-
-		//! todo terrain error as param
-		const meshMartini = tile.getMesh(terrainError);
-
-		const geometry = new BufferGeometry();
-
-		const vertices = [];
-		for (let i = 0; i < meshMartini.vertices.length / 2; i++) {
-			let x = meshMartini.vertices[i * 2],
-				y = meshMartini.vertices[i * 2 + 1];
-			vertices.push(x);
-			vertices.push(terrain[y * size + x] / 100);
-			vertices.push(y);
-		}
-
-		geometry.setIndex(new BufferAttribute(meshMartini.triangles, 1));
-		geometry.setAttribute('position', new BufferAttribute(new Float32Array(vertices), 3));
-
-		geometry.computeVertexNormals();
-		geometry.computeBoundingBox();
-		geometry.normalizeNormals();
-
-		return geometry;
-	};
 
 	let balls = [];
 
