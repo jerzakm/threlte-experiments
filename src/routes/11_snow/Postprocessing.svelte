c1fa1cb9dce2f14673557f5db728c4ef72852b3c
jerzakm <jerzakm@gmail.com>
2022-12-26 02:28:24 +0100

    postprocessing works

--- src/routes/11_snow/Postprocessing.svelte
+++ src/routes/11_snow/Postprocessing.svelte
@@ -3,7 +3,6 @@
 	import {
 		BloomEffect,
 		BrightnessContrastEffect,
-		ChromaticAberrationEffect,
 		EffectComposer,
 		EffectPass,
 		KernelSize,
@@ -12,15 +11,10 @@
 		SMAAPreset
 	} from 'postprocessing';
 	import { onDestroy } from 'svelte';
-	import { tweened } from 'svelte/motion';
-	import { Scene, Vector2 } from 'three';
-
-	export let scene: Scene;
-	export let camera: any;
 
 	const ctx = useThrelte();
 
-	const { renderer } = ctx;
+	const { renderer, camera, scene } = ctx;
 
 	let bloomEffect: BloomEffect | undefined = undefined;
 
@@ -30,46 +24,38 @@
 	const addComposerAndPasses = () => {
 		composer.removeAllPasses();
 
-		composer.addPass(new RenderPass(scene, camera));
-		// bloomEffect = new BloomEffect({
-		// 	intensity: 0.2,
-		// 	luminanceThreshold: 0.15,
-		// 	height: 512,
-		// 	width: 512,
-		// 	luminanceSmoothing: 0.08,
-		// 	mipmapBlur: true,
-		// 	kernelSize: KernelSize.MEDIUM
-		// });
-		// bloomEffect.luminancePass.enabled = true;
-		// (bloomEffect as any).ignoreBackground = true;
-		// composer.addPass(new EffectPass(camera, bloomEffect));
-		// composer.addPass(
-		// 	new EffectPass(
-		// 		camera,
-		// 		new ChromaticAberrationEffect({
-		// 			offset: new Vector2(0.0005, 0.0005),
-		// 			modulationOffset: 0,
-		// 			radialModulation: false
-		// 		})
-		// 	)
-		// );
-		// composer.addPass(
-		// 	new EffectPass(
-		// 		camera,
-		// 		new BrightnessContrastEffect({
-		// 			brightness: 0,
-		// 			contrast: 0.1
-		// 		})
-		// 	)
-		// );
-		// composer.addPass(
-		// 	new EffectPass(
-		// 		camera,
-		// 		new SMAAEffect({
-		// 			preset: SMAAPreset.LOW
-		// 		})
-		// 	)
-		// );
+		composer.addPass(new RenderPass(scene, $camera));
+		bloomEffect = new BloomEffect({
+			intensity: 1.0,
+			luminanceThreshold: 0.77,
+			height: 256,
+			width: 256,
+			luminanceSmoothing: 0.11,
+			mipmapBlur: false,
+			kernelSize: KernelSize.VERY_SMALL
+		});
+
+		composer.addPass(
+			new EffectPass(
+				$camera,
+				new BrightnessContrastEffect({
+					brightness: -0.1,
+					contrast: 0.1
+				})
+			)
+		);
+		composer.addPass(
+			new EffectPass(
+				$camera,
+				new SMAAEffect({
+					preset: SMAAPreset.HIGH
+				})
+			)
+		);
+
+		bloomEffect.luminancePass.enabled = true;
+		(bloomEffect as any).ignoreBackground = true;
+		// composer.addPass(new EffectPass($camera, bloomEffect));
 	};
 
 	$: if (renderer && camera && scene) {
